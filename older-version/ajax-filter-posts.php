b7981c3cc01ba2decaa1cc945ecc1020347857f4
Akhtarujjaman Shuvo <aktarujjman@gmail.com>
2023-03-06 01:31:19 +0600

    wip

--- older-version/ajax-filter-posts.php
+++ older-version/ajax-filter-posts.php
@@ -3,153 +3,153 @@
 // Defines
 define('AM_POST_GRID_VERSION', '3.3.0');
 
-/**
-* Loading Text Domain
-*/
-add_action('plugins_loaded', 'am_post_grid_plugin_loaded_action', 10, 2);
-function am_post_grid_plugin_loaded_action() {
-	load_plugin_textdomain( 'ajax-filter-posts', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
-}
+// /**
+// * Loading Text Domain
+// */
+// add_action('plugins_loaded', 'am_post_grid_plugin_loaded_action', 10, 2);
+// function am_post_grid_plugin_loaded_action() {
+// 	load_plugin_textdomain( 'ajax-filter-posts', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
+// }
 
 
-// Enqueue scripts
-function am_ajax_filter_posts_scripts(){
+// // Enqueue scripts
+// function am_ajax_filter_posts_scripts(){
 
-	// CSS File
-	wp_enqueue_style( 'asrafp-styles', plugin_dir_url( __FILE__ ) . 'assets/css/post-grid-styles.css', null, AM_POST_GRID_VERSION );
+// 	// CSS File
+// 	wp_enqueue_style( 'asrafp-styles', plugin_dir_url( __FILE__ ) . 'assets/css/post-grid-styles.css', null, AM_POST_GRID_VERSION );
 
-	// JS File
-	wp_register_script( 'asr_ajax_filter_post', plugin_dir_url( __FILE__ ) . 'assets/js/post-grid-scripts.js', array('jquery'), AM_POST_GRID_VERSION );
-	wp_enqueue_script( 'asr_ajax_filter_post' );
+// 	// JS File
+// 	wp_register_script( 'asr_ajax_filter_post', plugin_dir_url( __FILE__ ) . 'assets/js/post-grid-scripts.js', array('jquery'), AM_POST_GRID_VERSION );
+// 	wp_enqueue_script( 'asr_ajax_filter_post' );
 
-	// Localization
-	wp_localize_script( 'asr_ajax_filter_post', 'asr_ajax_params', array(
-	        'asr_ajax_nonce' => wp_create_nonce( 'asr_ajax_nonce' ),
-	        'asr_ajax_url' => admin_url( 'admin-ajax.php' ),
-	    )
-	);
-}
+// 	// Localization
+// 	wp_localize_script( 'asr_ajax_filter_post', 'asr_ajax_params', array(
+// 	        'asr_ajax_nonce' => wp_create_nonce( 'asr_ajax_nonce' ),
+// 	        'asr_ajax_url' => admin_url( 'admin-ajax.php' ),
+// 	    )
+// 	);
+// }
 
-add_action( 'wp_enqueue_scripts', 'am_ajax_filter_posts_scripts' );
+// add_action( 'wp_enqueue_scripts', 'am_ajax_filter_posts_scripts' );
 
-//shortcode function
-function am_post_grid_shortcode_mapper( $atts, $content = null ) {
+// //shortcode function
+// function am_post_grid_shortcode_mapper( $atts, $content = null ) {
 
-	// Posts per pages.
-	$posts_per_page = ( get_option( 'posts_per_page', true ) ) ? get_option( 'posts_per_page', true ) : 9;
+// 	// Posts per pages.
+// 	$posts_per_page = ( get_option( 'posts_per_page', true ) ) ? get_option( 'posts_per_page', true ) : 9;
 
-	// Default attributes
-	$shortcode_atts = shortcode_atts(
-            array(
-                'show_filter' 		=> "yes",
-                'btn_all' 			=> "yes",
-                'initial' 			=> "-1",
-                'layout' 			=> '1',
-                'post_type' 		=> 'post',
-                'posts_per_page' 	=> $posts_per_page,
-                'cat' 				=> '',
-                'terms' 			=> '',
-                'paginate' 			=> 'no',
-                'hide_empty' 		=> 'true',
-                'orderby' 			=> 'menu_order date', //Display posts sorted by ‘menu_order’ with a fallback to post ‘date’
-    			'order'   			=> 'DESC',
-    			'pagination_type'   => '',
-    			'infinite_scroll'   => '',
-    			'animation'  		=> '',
-    			'grid_id'  			=> '', // master ID
-            ),
-            $atts
-        );
+// 	// Default attributes
+// 	$shortcode_atts = shortcode_atts(
+//             array(
+//                 'show_filter' 		=> "yes",
+//                 'btn_all' 			=> "yes",
+//                 'initial' 			=> "-1",
+//                 'layout' 			=> '1',
+//                 'post_type' 		=> 'post',
+//                 'posts_per_page' 	=> $posts_per_page,
+//                 'cat' 				=> '',
+//                 'terms' 			=> '',
+//                 'paginate' 			=> 'no',
+//                 'hide_empty' 		=> 'true',
+//                 'orderby' 			=> 'menu_order date', //Display posts sorted by ‘menu_order’ with a fallback to post ‘date’
+//     			'order'   			=> 'DESC',
+//     			'pagination_type'   => '',
+//     			'infinite_scroll'   => '',
+//     			'animation'  		=> '',
+//     			'grid_id'  			=> '', // master ID
+//             ),
+//             $atts
+//         );
 
-    // Params extraction
-    extract($shortcode_atts);
+//     // Params extraction
+//     extract($shortcode_atts);
 
-	ob_start();
+// 	ob_start();
 
-	// Texonomy arguments
-	$taxonomy = 'category';
-	$args = array(
-		'hide_empty' => $hide_empty,
-	    'taxonomy' => $taxonomy,
-	    'include' => $terms ? $terms : $cat,
-	);
+// 	// Texonomy arguments
+// 	$taxonomy = 'category';
+// 	$args = array(
+// 		'hide_empty' => $hide_empty,
+// 	    'taxonomy' => $taxonomy,
+// 	    'include' => $terms ? $terms : $cat,
+// 	);
 
-	// Get category terms
-	$terms = get_terms($args); ?>
-	<div class="am_ajax_post_grid_wrap" data-pagination_type="<?php echo esc_attr($pagination_type); ?>" data-am_ajax_post_grid='<?php echo json_encode($shortcode_atts);?>'>
+// 	// Get category terms
+// 	$terms = get_terms($args); ?>
+<!-- // 	<div class="am_ajax_post_grid_wrap" data-pagination_type="<?php echo esc_attr($pagination_type); ?>" data-am_ajax_post_grid='<?php echo json_encode($shortcode_atts);?>'>
 
-		<?php if ( $show_filter == "yes" && $terms && !is_wp_error( $terms ) ){ ?>
-			<div class="asr-filter-div" data-layout="<?php echo $layout; ?>"><ul>
-				<?php if($btn_all != "no"): ?>
-					<li class="asr_texonomy" data_id="-1"><?php echo esc_html('All','ajax-filter-posts'); ?></li>
-				<?php endif; ?>
-		        <?php foreach( $terms as $term ) { ?>
-		            <li class="asr_texonomy" data_id="<?php echo $term->term_id; ?>"><?php echo $term->name; ?></li>
-		        <?php } ?>
-	        </ul></div>
-	    <?php } ?>
+// 		<?php if ( $show_filter == "yes" && $terms && !is_wp_error( $terms ) ){ ?>
+// 			<div class="asr-filter-div" data-layout="<?php echo $layout; ?>"><ul>
+// 				<?php if($btn_all != "no"): ?>
+// 					<li class="asr_texonomy" data_id="-1"><?php echo esc_html('All','ajax-filter-posts'); ?></li>
+// 				<?php endif; ?>
+// 		        <?php foreach( $terms as $term ) { ?>
+// 		            <li class="asr_texonomy" data_id="<?php echo $term->term_id; ?>"><?php echo $term->name; ?></li>
+// 		        <?php } ?>
+// 	        </ul></div>
+// 	    <?php } ?>
 
-	    <div class="asr-ajax-container">
-		    <div class="asr-loader">
-		    	<div class="lds-dual-ring"></div>
-		    </div>
-		    <div class="asrafp-filter-result">
-		    	<?php echo am_post_grid_output( am_post_grid_get_args_from_shortcode_atts($shortcode_atts) ); ?>
-		    </div>
-	    </div>
-    </div>
+// 	    <div class="asr-ajax-container">
+// 		    <div class="asr-loader">
+// 		    	<div class="lds-dual-ring"></div>
+// 		    </div>
+// 		    <div class="asrafp-filter-result">
+// 		    	<?php echo am_post_grid_output( am_post_grid_get_args_from_shortcode_atts($shortcode_atts) ); ?>
+// 		    </div>
+// 	    </div>
+//     </div> -->
 
-	<?php return ob_get_clean();
-}
-add_shortcode('asr_ajax','am_post_grid_shortcode_mapper');
-add_shortcode('am_post_grid','am_post_grid_shortcode_mapper');
+// 	<?php return ob_get_clean();
+// }
+// add_shortcode('asr_ajax','am_post_grid_shortcode_mapper');
+// add_shortcode('am_post_grid','am_post_grid_shortcode_mapper');
 
 // Get Args from Json Data
-function am_post_grid_get_args_from_shortcode_atts( $jsonData ){
+// function am_post_grid_get_args_from_shortcode_atts( $jsonData ){
 
-	if( isset( $jsonData['posts_per_page'] ) ){
-		$data['posts_per_page'] = intval( $jsonData['posts_per_page'] );
-	}
+// 	if( isset( $jsonData['posts_per_page'] ) ){
+// 		$data['posts_per_page'] = intval( $jsonData['posts_per_page'] );
+// 	}
 
-	if( isset( $jsonData['orderby'] ) ){
-		$data['orderby'] = sanitize_text_field( $jsonData['orderby'] );
-	}
+// 	if( isset( $jsonData['orderby'] ) ){
+// 		$data['orderby'] = sanitize_text_field( $jsonData['orderby'] );
+// 	}
 
-	if( isset( $jsonData['order'] ) ){
-		$data['order'] = sanitize_text_field( $jsonData['order'] );
-	}
+// 	if( isset( $jsonData['order'] ) ){
+// 		$data['order'] = sanitize_text_field( $jsonData['order'] );
+// 	}
 
-	if( isset( $jsonData['pagination_type'] ) ){
-		$data['pagination_type'] = sanitize_text_field( $jsonData['pagination_type'] );
+// 	if( isset( $jsonData['pagination_type'] ) ){
+// 		$data['pagination_type'] = sanitize_text_field( $jsonData['pagination_type'] );
 		
-	}
+// 	}
 
-	if( isset( $jsonData['animation'] ) && $jsonData['animation'] == "true" ){
-		$data['animation'] = 'am_has_animation';
-	}
+// 	if( isset( $jsonData['animation'] ) && $jsonData['animation'] == "true" ){
+// 		$data['animation'] = 'am_has_animation';
+// 	}
 
-	if( isset( $jsonData['infinite_scroll'] ) && $jsonData['infinite_scroll'] == "true" ){
-		$data['infinite_scroll'] = 'infinite_scroll';
-	}
+// 	if( isset( $jsonData['infinite_scroll'] ) && $jsonData['infinite_scroll'] == "true" ){
+// 		$data['infinite_scroll'] = 'infinite_scroll';
+// 	}
 
-	// Bind to Category Terms
-	$terms =  '';
-	if ( isset( $jsonData['cat'] ) && !empty( $jsonData['cat'] ) ) {
-		$terms = explode(',', $jsonData['cat']);
-	} elseif ( isset( $jsonData['terms'] ) && !empty( $jsonData['terms'] ) ) {
-		$terms = explode(',', $jsonData['terms']);
-	}
+// 	// Bind to Category Terms
+// 	$terms =  '';
+// 	if ( isset( $jsonData['cat'] ) && !empty( $jsonData['cat'] ) ) {
+// 		$terms = explode(',', $jsonData['cat']);
+// 	} elseif ( isset( $jsonData['terms'] ) && !empty( $jsonData['terms'] ) ) {
+// 		$terms = explode(',', $jsonData['terms']);
+// 	}
 
 	
-	// Tax Query
-	if ( !empty( $terms ) ) {
-		$data['tax_query'] = [
-			'category' => $terms,
-		];
-	}
+// 	// Tax Query
+// 	if ( !empty( $terms ) ) {
+// 		$data['tax_query'] = [
+// 			'category' => $terms,
+// 		];
+// 	}
 
-	return $data;
-}
+// 	return $data;
+// }
 
 // Load Posts Ajax actions
 add_action('wp_ajax_asr_filter_posts', 'am_post_grid_load_posts_ajax_functions');
@@ -346,16 +346,3 @@ function am_post_grid_output( $args = [] ){
 }
 
 
-/**
- * Add plugin action links.
- *
- * @since 1.0.0
- * @version 4.0.0
- */
-function am_ajax_post_grid_plugin_action_links( $links ) {
-	$plugin_links = array(
-		'<a href="'.admin_url( 'admin.php?page=ajax-post-grid' ).'">' . esc_html__( 'Options', 'ajax-filter-posts' ) . '</a>',
-	);
-	return array_merge( $plugin_links, $links );
-}
-//add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'am_ajax_post_grid_plugin_action_links' );
