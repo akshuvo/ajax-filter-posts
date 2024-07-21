839ed8e02a7b004168eb02f292c67e0e716d404e
Akhtarujjaman Shuvo <aktarujjman@gmail.com>
2024-07-22 02:59:04 +0600

    welcome

--- admin/views/welcome.php
+++ admin/views/welcome.php
@@ -9,8 +9,8 @@
 							<span class="dashicons dashicons-editor-help"></span>
 						</div>
 						<div class="gm-card-containt">
-							<h4><?php echo esc_html( 'Custom Post Type & Taxonomy', 'gridmaster' ); ?></h4>
-							<p> <?php echo esc_html( 'With GridMaster, effortlessly display any custom post types and taxonomies on your post grid.', 'gridmaster' ); ?> </p>
+							<h4><?php esc_html_e( 'Custom Post Type & Taxonomy', 'gridmaster' ); ?></h4>
+							<p> <?php esc_html_e( 'With GridMaster, effortlessly display any custom post types and taxonomies on your post grid.', 'gridmaster' ); ?> </p>
 						</div>
 					</div>
 				</div>
@@ -22,8 +22,8 @@
 							<span class="dashicons dashicons-editor-help"></span>
 						</div>
 						<div class="gm-card-containt">
-							<h4><?php echo esc_html( 'Pre-Built Grid and Filter Styles', 'gridmaster' ); ?></h4>
-							<p> <?php echo esc_html( 'GridMaster features various pre-designed grid and filter styles for a quick, stylish setup. No custom design needed.', 'gridmaster' ); ?> </p>
+							<h4><?php esc_html_e( 'Pre-Built Grid and Filter Styles', 'gridmaster' ); ?></h4>
+							<p> <?php esc_html_e( 'GridMaster features various pre-designed grid and filter styles for a quick, stylish setup. No custom design needed.', 'gridmaster' ); ?> </p>
 						</div>
 					</div>
 				</div>
@@ -35,8 +35,8 @@
 							<span class="dashicons dashicons-editor-help"></span>
 						</div>
 						<div class="gm-card-containt">
-							<h4><?php echo esc_html( 'Versatile Pagination Options', 'gridmaster' ); ?></h4>
-							<p> <?php echo esc_html( 'GridMaster offers a variety of pagination options to help you display your content in the most effective way.', 'gridmaster' ); ?> </p>
+							<h4><?php esc_html_e( 'Versatile Pagination Options', 'gridmaster' ); ?></h4>
+							<p> <?php esc_html_e( 'GridMaster offers a variety of pagination options to help you display your content in the most effective way.', 'gridmaster' ); ?> </p>
 						</div>
 					</div>
 				</div>
@@ -45,11 +45,11 @@
 		<!-- Key Features  -->
 
 		<div class="pb-3 pt-4 text-center">
-			<a class="gm-btn gm-btn-fill" href="<?php echo esc_url( admin_url( 'admin.php?page=gridmaster&path=build-grid' ) ); ?>"><?php echo esc_html( 'Build Your First Grid', 'gridmaster' ); ?></a>
+			<a class="gm-btn gm-btn-fill" href="<?php echo esc_url( admin_url( 'admin.php?page=gridmaster&path=build-grid' ) ); ?>"><?php esc_html_e( 'Build Your First Grid', 'gridmaster' ); ?></a>
 		</div>
 
 		<div class="pb-3 pt-4 text-center">
-			<h2><?php echo esc_html( 'Why GridMaster is Different?', 'gridmaster' ); ?></h2>
+			<h2><?php esc_html_e( 'Why GridMaster is Different?', 'gridmaster' ); ?></h2>
 		</div>
 
 		<!-- Code Features  -->
@@ -61,8 +61,8 @@
 							<span class="dashicons dashicons-editor-help"></span>
 						</div>
 						<div class="gm-card-containt">
-							<h4><?php echo esc_html( 'Seamless Efficiency with Clean Code', 'gridmaster' ); ?></h4>
-							<p> <?php echo esc_html( 'GridMaster is built with clean, well-organized code to ensure seamless efficiency and performance.', 'gridmaster' ); ?> </p>
+							<h4><?php esc_html_e( 'Seamless Efficiency with Clean Code', 'gridmaster' ); ?></h4>
+							<p> <?php esc_html_e( 'GridMaster is built with clean, well-organized code to ensure seamless efficiency and performance.', 'gridmaster' ); ?> </p>
 						</div>
 					</div>
 				</div>
@@ -74,8 +74,8 @@
 							<span class="dashicons dashicons-editor-help"></span>
 						</div>
 						<div class="gm-card-containt">
-							<h4><?php echo esc_html( 'Frontend-Backend Code Balance', 'gridmaster' ); ?></h4>
-							<p> <?php echo esc_html( 'We don\'t clutter your frontend with backend code. GridMaster maintains a lean codebase for optimal site performance.', 'gridmaster' ); ?> </p>
+							<h4><?php esc_html_e( 'Frontend-Backend Code Balance', 'gridmaster' ); ?></h4>
+							<p> <?php esc_html_e( 'We don\'t clutter your frontend with backend code. GridMaster maintains a lean codebase for optimal site performance.', 'gridmaster' ); ?> </p>
 						</div>
 					</div>
 				</div>
@@ -87,8 +87,8 @@
 							<span class="dashicons dashicons-editor-help"></span>
 						</div>
 						<div class="gm-card-containt">
-							<h4><?php echo esc_html( 'Customizable with Shortcode Builder', 'gridmaster' ); ?></h4>
-							<p> <?php echo esc_html( 'Shortcode Builder lets you place post grids anywhere on your site. Plus, build your custom theme with GridMaster with ease.', 'gridmaster' ); ?> </p>
+							<h4><?php esc_html_e( 'Customizable with Shortcode Builder', 'gridmaster' ); ?></h4>
+							<p> <?php esc_html_e( 'Shortcode Builder lets you place post grids anywhere on your site. Plus, build your custom theme with GridMaster with ease.', 'gridmaster' ); ?> </p>
 						</div>
 					</div>
 				</div>
@@ -100,8 +100,8 @@
 							<span class="dashicons dashicons-editor-help"></span>
 						</div>
 						<div class="gm-card-containt">
-							<h4><?php echo esc_html( 'Works with Any Theme', 'gridmaster' ); ?></h4>
-							<p> <?php echo esc_html( 'GridMaster is compatible with any WordPress theme. No need to change your theme to use GridMaster.', 'gridmaster' ); ?> </p>
+							<h4><?php esc_html_e( 'Works with Any Theme', 'gridmaster' ); ?></h4>
+							<p> <?php esc_html_e( 'GridMaster is compatible with any WordPress theme. No need to change your theme to use GridMaster.', 'gridmaster' ); ?> </p>
 						</div>
 					</div>
 				</div>
@@ -113,16 +113,16 @@
 							<span class="dashicons dashicons-editor-help"></span>
 						</div>
 						<div class="gm-card-containt">
-							<h4><?php echo esc_html( 'Essential Features at No Cost', 'gridmaster' ); ?></h4>
-							<p> <?php echo esc_html( 'GridMaster comes packed with nearly all necessary features as part of its free package, ensuring exceptional value.', 'gridmaster' ); ?> </p>
+							<h4><?php esc_html_e( 'Essential Features at No Cost', 'gridmaster' ); ?></h4>
+							<p> <?php esc_html_e( 'GridMaster comes packed with nearly all necessary features as part of its free package, ensuring exceptional value.', 'gridmaster' ); ?> </p>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="align-content-center d-flex flex-column flex-wrap h-100 justify-content-center text-center">
-					<h4><?php echo esc_html( 'Still Not Convinced?', 'gridmaster' ); ?></h4>
-					<a class="gm-btn gm-btn-fill" href="<?php echo esc_url( gridmaster_website_url( 'gridmaster/' ) ); ?>" target="_blank"><span class="dashicons dashicons-external"></span> <?php echo esc_html( 'Check Out the Pro Version', 'gridmaster' ); ?></a>
+					<h4><?php esc_html_e( 'Still Not Convinced?', 'gridmaster' ); ?></h4>
+					<a class="gm-btn gm-btn-fill" href="<?php echo esc_url( gridmaster_website_url( 'gridmaster/' ) ); ?>" target="_blank"><span class="dashicons dashicons-external"></span> <?php esc_html_e( 'Check Out the Pro Version', 'gridmaster' ); ?></a>
 				</div>
 			</div>
 		</div>
@@ -131,23 +131,21 @@
 		<div class="gm-seperator pt-4"></div>
 
 		<div class="gm-support-container mt-5 p-5 text-center">
-			<h2 class="m-0"><?php echo esc_html( 'Encounter a Puzzle? Raise a Support Ticket', 'gridmaster' ); ?></h2>
-			<p><?php echo esc_html( 'Should you need guidance or assistance, don\'t hesitate to create a support ticket. Our team is ready to solve your GridMaster mysteries.', 'gridmaster' ); ?> </p>
+			<h2 class="m-0"><?php esc_html_e( 'Encounter a Puzzle? Raise a Support Ticket', 'gridmaster' ); ?></h2>
+			<p><?php esc_html_e( 'Should you need guidance or assistance, don\'t hesitate to create a support ticket. Our team is ready to solve your GridMaster mysteries.', 'gridmaster' ); ?> </p>
 			<div class="gm-support-btns mt-4 pt-1">
-				<a href="https://wordpress.org/support/plugin/ajax-filter-posts/" class="gm-btn gm-btn-fill-hover" target="_blank"><?php echo esc_html( 'Support Forum', 'gridmaster' ); ?></a>
-				<a href="<?php echo esc_url( gridmaster_website_url( 'submit-a-ticket/' ) ); ?>" class="gm-btn gm-btn-fill-hover gm-btn gm-btn-fill-hover-secondary" target="_blank"><?php echo esc_html( 'Submit a Ticket', 'gridmaster' ); ?></a>
-				<a href="<?php echo esc_url( gridmaster_website_url( 'docs-category/gridmaster/' ) ); ?>" class="gm-btn gm-btn-fill-hover gm-btn-secondary" target="_blank"><?php echo esc_html( 'Documentation', 'gridmaster' ); ?></a>
+				<a href="https://wordpress.org/support/plugin/ajax-filter-posts/" class="gm-btn gm-btn-fill-hover" target="_blank"><?php esc_html_e( 'Support Forum', 'gridmaster' ); ?></a>
+				<a href="<?php echo esc_url( gridmaster_website_url( 'submit-a-ticket/' ) ); ?>" class="gm-btn gm-btn-fill-hover gm-btn gm-btn-fill-hover-secondary" target="_blank"><?php esc_html_e( 'Submit a Ticket', 'gridmaster' ); ?></a>
+				<a href="<?php echo esc_url( gridmaster_website_url( 'docs-category/gridmaster/' ) ); ?>" class="gm-btn gm-btn-fill-hover gm-btn-secondary" target="_blank"><?php esc_html_e( 'Documentation', 'gridmaster' ); ?></a>
 			</div>
 		</div>
 
 		<div class="align-items-center d-flex gm-livechat-container gm-support-container justify-content-between mt-5 px-5 py-4 gm-border-primary gm-border-width-2x">
 			<div class="">
-				<h2 class="m-0"><?php echo esc_html( 'GridMaster Pro? Access Instant Support!', 'gridmaster' ); ?></h2>
-				<p class="m-0"><?php echo esc_html( 'As a GridMaster Pro user, take advantage of our Live Chat feature for immediate help and assistance.', 'gridmaster' ); ?> </p>
+				<h2 class="m-0"><?php esc_html_e( 'GridMaster Pro? Access Instant Support!', 'gridmaster' ); ?></h2>
+				<p class="m-0"><?php esc_html_e( 'As a GridMaster Pro user, take advantage of our Live Chat feature for immediate help and assistance.', 'gridmaster' ); ?> </p>
 			</div>
-			<a href="<?php echo esc_url( gridmaster_website_url( 'live-chat/' ) ); ?>" class="gm-btn gm-btn-fill-hover" target="_blank"><?php echo esc_html( 'Live Chat', 'gridmaster' ); ?></a>
+			<a href="<?php echo esc_url( gridmaster_website_url( 'live-chat/' ) ); ?>" class="gm-btn gm-btn-fill-hover" target="_blank"><?php esc_html_e( 'Live Chat', 'gridmaster' ); ?></a>
 		</div>
-	
-		
 	</div> 
 </div>
